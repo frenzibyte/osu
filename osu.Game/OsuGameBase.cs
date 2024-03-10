1d91a66a06ebac05bbd5cdffe9038f2134e38a35
Salman Ahmed <frenzibyte@gmail.com>
2024-03-11 02:34:20 +0300

    Use development server

--- osu.Game/OsuGameBase.cs
+++ osu.Game/OsuGameBase.cs
@@ -98,7 +98,7 @@ namespace osu.Game
         /// </summary>
         private const double global_track_volume_adjust = 0.8;
 
-        public virtual bool UseDevelopmentServer => DebugUtils.IsDebugBuild;
+        public virtual bool UseDevelopmentServer => true;
 
         public virtual EndpointConfiguration CreateEndpoints() =>
             UseDevelopmentServer ? new DevelopmentEndpointConfiguration() : new ProductionEndpointConfiguration();
