685d5598ad66180d43a5ed3c5466fda3d843ad10
Salman Alshamrani <frenzibyte@gmail.com>
2025-01-26 01:34:03 -0500

    Add basic beatmap bindings, add beatmap info wedge, and display beatmap background

--- osu.Game/Screens/SelectV2/SongSelectV2.cs
+++ osu.Game/Screens/SelectV2/SongSelectV2.cs
@@ -4,15 +4,22 @@
 using System;
 using System.Collections.Generic;
 using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Colour;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
 using osu.Framework.Screens;
+using osu.Game.Beatmaps;
+using osu.Game.Configuration;
 using osu.Game.Overlays;
 using osu.Game.Overlays.Mods;
 using osu.Game.Screens.Footer;
 using osu.Game.Screens.Menu;
 using osu.Game.Screens.Play;
 using osu.Game.Screens.SelectV2.Footer;
+using osuTK.Graphics;
 
 namespace osu.Game.Screens.SelectV2
 {
@@ -20,9 +27,13 @@ namespace osu.Game.Screens.SelectV2
     /// This screen is intended to house all components introduced in the new song select design to add transitions and examine the overall look.
     /// This will be gradually built upon and ultimately replace <see cref="Select.SongSelect"/> once everything is in place.
     /// </summary>
-    public partial class SongSelectV2 : OsuScreen
+    public partial class SongSelectV2 : ScreenWithBeatmapBackground
     {
         private const float logo_scale = 0.4f;
+
+        protected const float BACKGROUND_BLUR = 20;
+
+        private BeatmapInfoWedgeV2 beatmapInfoWedge = null!;
 
         private readonly ModSelectOverlay modSelectOverlay = new SoloModSelectOverlay();
 
@@ -34,14 +45,7 @@ namespace osu.Game.Screens.SelectV2
         [Resolved]
         private OsuLogo? logo { get; set; }
 
-        [BackgroundDependencyLoader]
-        private void load()
-        {
-            AddRangeInternal(new Drawable[]
-            {
-                modSelectOverlay,
-            });
-        }
+        private Bindable<bool> configBackgroundBlur = null!;
 
         public override IReadOnlyList<ScreenFooterButton> CreateFooterButtons() => new ScreenFooterButton[]
         {
@@ -50,40 +54,65 @@ namespace osu.Game.Screens.SelectV2
             new ScreenFooterButtonOptions(),
         };
 
-        protected override void LoadComplete()
+        [BackgroundDependencyLoader]
+        private void load(OsuConfigManager config)
         {
-            base.LoadComplete();
-
-            modSelectOverlay.State.BindValueChanged(v =>
+            configBackgroundBlur = config.GetBindable<bool>(OsuSetting.SongSelectBackgroundBlur);
+            configBackgroundBlur.BindValueChanged(_ =>
             {
-                logo?.ScaleTo(v.NewValue == Visibility.Visible ? 0f : logo_scale, 400, Easing.OutQuint)
-                    .FadeTo(v.NewValue == Visibility.Visible ? 0f : 1f, 200, Easing.OutQuint);
-            }, true);
+                if (this.IsCurrentScreen())
+                    updateScreenBackground();
+            });
+
+            AddRangeInternal(new Drawable[]
+            {
+                new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Colour = ColourInfo.GradientVertical(Color4.Black, Color4.Black.Opacity(0f)),
+                    Height = (float)Math.Sqrt(0.5f),
+                },
+                beatmapInfoWedge = new BeatmapInfoWedgeV2(),
+                modSelectOverlay,
+            });
         }
 
         public override void OnEntering(ScreenTransitionEvent e)
         {
+            base.OnEntering(e);
+
             this.FadeIn();
 
+            Beatmap.BindValueChanged(onBeatmapChanged, true);
+
+            modSelectOverlay.State.BindValueChanged(onModSelectStateChanged, true);
             modSelectOverlay.SelectedMods.BindTo(Mods);
 
-            base.OnEntering(e);
+            beatmapInfoWedge.Show();
+
+            updateScreenBackground();
         }
 
         public override void OnResuming(ScreenTransitionEvent e)
         {
+            base.OnResuming(e);
+
             this.FadeIn();
+
+            beatmapInfoWedge.Show();
 
             // required due to https://github.com/ppy/osu-framework/issues/3218
             modSelectOverlay.SelectedMods.Disabled = false;
             modSelectOverlay.SelectedMods.BindTo(Mods);
 
-            base.OnResuming(e);
+            updateScreenBackground();
         }
 
         public override void OnSuspending(ScreenTransitionEvent e)
         {
-            this.Delay(400).FadeOut();
+            this.FadeOut(400, Easing.OutQuint);
+
+            beatmapInfoWedge.Hide();
 
             modSelectOverlay.SelectedMods.UnbindFrom(Mods);
 
@@ -92,7 +121,10 @@ namespace osu.Game.Screens.SelectV2
 
         public override bool OnExiting(ScreenExitEvent e)
         {
-            this.Delay(400).FadeOut();
+            this.FadeOut(400, Easing.OutQuint);
+
+            beatmapInfoWedge.Hide();
+
             return base.OnExiting(e);
         }
 
@@ -131,6 +163,34 @@ namespace osu.Game.Screens.SelectV2
             Scheduler.AddDelayed(() => Footer?.StopTrackingLogo(), 120);
             logo.ScaleTo(0.2f, 120, Easing.Out);
             logo.FadeOut(120, Easing.Out);
+        }
+
+        private void onBeatmapChanged(ValueChangedEvent<WorkingBeatmap> b)
+        {
+            if (this.IsCurrentScreen())
+                updateScreenBackground();
+
+            beatmapInfoWedge.Beatmap = b.NewValue;
+        }
+
+        private void updateScreenBackground()
+        {
+            ApplyToBackground(backgroundModeBeatmap =>
+            {
+                backgroundModeBeatmap.Beatmap = Beatmap.Value;
+                backgroundModeBeatmap.BlurAmount.Value = configBackgroundBlur.Value ? BACKGROUND_BLUR : 0f;
+                backgroundModeBeatmap.DimWhenUserSettingsIgnored.Value = configBackgroundBlur.Value ? 0 : 0.4f;
+                backgroundModeBeatmap.IgnoreUserSettings.Value = true;
+                backgroundModeBeatmap.FadeColour(Color4.White, 250);
+            });
+        }
+
+        private void onModSelectStateChanged(ValueChangedEvent<Visibility> v)
+        {
+            if (v.NewValue == Visibility.Visible)
+                logo?.ScaleTo(0f, 400, Easing.OutQuint).FadeTo(0f, 200, Easing.OutQuint);
+            else
+                logo?.ScaleTo(logo_scale, 400, Easing.OutQuint).FadeTo(1f, 200, Easing.OutQuint);
         }
 
         private partial class SoloModSelectOverlay : UserModSelectOverlay
